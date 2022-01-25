120d2ad6732f40bdaf3f4f59b5ea700545e13226
BHUVAN-CHANDRA-JOSHI <bhuvanjoshi923@gmail.com>
2022-01-25 18:06:30 +0530

    done data in dynamically

--- src/components/dashboard/AllPages/createpaymentlink/PayerDetails.js
+++ src/components/dashboard/AllPages/createpaymentlink/PayerDetails.js
@@ -13,10 +13,11 @@ const PayerDetails = () => {
         customer_type: ""
     }
     const { user } = useSelector((state) => state.auth);
-    const [data, setData] = React.useState([])
+    const [data, setData] = React.useState([initialState])
     var clientMerchantDetailsList = user.clientMerchantDetailsList;
     const { clientCode } = clientMerchantDetailsList[0];
     console.log(clientMerchantDetailsList);
+    //console.log(clientCode)
 
 
 
@@ -24,13 +25,16 @@ const PayerDetails = () => {
 
 
     const getFileName = async () => {
+        // console.log(clientCode,'hello')
         await axios(`https://paybylink.sabpaisa.in/paymentlink/getCustomers/${clientCode}`)  //MPSE1
             .then(res => {
-                console.log(res.data)
+            
+                console.log(res)
                 setData(res.data);
             })
             .catch(err => {
                 console.log(err)
+                
             });
 
     }
@@ -40,6 +44,30 @@ const PayerDetails = () => {
     React.useEffect(() => {
         getFileName();
     }, []);
+
+
+   const onClick= async()=>{
+    
+        const response = await axios.post('https://paybylink.sabpaisa.in/paymentlink/addCustomers')
+       
+           
+           
+   
+     
+       .then((response) => {  
+        console.warn(response);
+      setData(response.data);
+           
+         console.log(JSON.stringify(response.data));
+       })
+       .catch((error) => { 
+         console.log(error);
+       }
+
+       )}
+       const submitHandler={
+
+       }
 
 
 
@@ -60,7 +88,7 @@ const PayerDetails = () => {
                             </button>
                         </div>
                         <div class="modal-body">
-                            <form>
+                            <form  onSubmit={submitHandler}> 
                                 <div class="form-group">
                                     <label for="recipient-name" class="col-form-label">Name of Payer:</label>
                                     <input type="text" placeholder="Enter Name of Payer" class="form-control" id="recipient-name" />
@@ -92,14 +120,15 @@ const PayerDetails = () => {
 
 
                                 </div>
+                                <div class="modal-footer">
+                            <button  onClick={onClick}  type="button" class="btn btn-primary" >Submit</button>
+                            <button type="button" class="btn btn-danger">Update</button>
+                            <button type="button" class="btn btn-primary" data-dismiss="modal">Cancel</button>
+                        </div>
 
                             </form>
                         </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-primary" >Add user</button>
-                            <button type="button" class="btn btn-danger">Send message</button>
-                            <button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>
-                        </div>
+                       
                     </div>
                 </div>
             </div>
@@ -137,7 +166,9 @@ const PayerDetails = () => {
             </table>
 
  */}
-            <table class="table table-striped" style={{ position: 'absolute', top: 450, left: 300, width: 800 }}>
+           <div class="full-screen-scroller">
+
+            <table data-spy="scroll"  data-offset="50" class="table table-striped" style={{ position: 'absolute', top: 450, left: 300, width: 800, height:200}}>
                 <thead>
                     <tr>
                         <th scope='col'>Pair Name</th>
@@ -146,6 +177,7 @@ const PayerDetails = () => {
                         <th scope='col'>Payer  Category</th>
                         <th scope='col'>Edit</th>
                         <th scope='col'>Delete</th>
+                
                         <th scope='col'>Action</th>
                     </tr>
                 </thead>
@@ -154,9 +186,9 @@ const PayerDetails = () => {
                          {data.map((user) => ( 
                             <tr>
                                 <td>{user.name}</td>
-                                <td>8920885489</td>
-                                <td>rahul.singh@srslive.in</td>
-                                <td>customer</td>
+                                <td>{user.phone_number}</td>
+                                <td>{user.email}</td>
+                                <td>{user.customer_type}</td>
                                 <td>
                                     <button class="btn bhuvi btn-primary mt-2 "  >Edit</button>
 
@@ -172,6 +204,8 @@ const PayerDetails = () => {
                 </tbody>
             </table>
         </div>
+        </div>
+       
 
     )
 };
