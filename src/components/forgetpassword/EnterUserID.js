18afa8a40fc95f284457c69e1a0ae3cd5040849d
harrisfazal <harrisfazal@ymail.com>
2022-02-23 23:56:43 +0530

    Added Validations in Forget Partialy Done

--- src/components/forgetpassword/EnterUserID.js
+++ src/components/forgetpassword/EnterUserID.js
@@ -1,41 +1,99 @@
-import React from 'react'
+import React from "react";
+import { Formik, Field, Form, ErrorMessage } from "formik";
+import * as Yup from "yup";
 
-function EnterUserID(props) {
+const EnterUserID = (props) => {
+  const { handleFormSubmit } = props;
 
-    // const {handleFormSubmit} = props;
-    function handleSubmit(e) {
-        e.preventDefault();      
-        console.log('You clicked submit.');
-      }
-    
+  const validationSchema = Yup.object().shape({
+    Email: Yup.string().email("Enter valid email").required("Required"),
+  });
+
+  const handleSubmit = () => {
+    console.log("You clicked");
+  };
+
+  const initialValues = {
+    Email: "",
+  };
+
   return (
-   <div className="container-fluid bg-info">
-                <div className="row " >
-                <div className="col-sm-6 mx-auto">
-                    <div className="card ">
-                    <div className="card-header text-center">
-                        Forget Password
-                    </div>
-                    <div className="card-body">
-                        <h5 className="card-title">Please Enter the detatils. </h5>
-                        <form onSubmit={handleSubmit}>
-                        <div className="form-group">
-                            <label htmlFor="exampleInputEmail1">Email address</label>
-                            <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
-                            <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
-                        </div>
-                        <button className="btn btn-primary"  onClick={()=>props.props('a2')} >Submit</button>
-                        </form>
-                        <p className="card-text" style={{display: "none"}}>With supporting text below as a natural lead-in to additional content.</p>
-                    </div>
-                    <div className="card-footer text-muted text-center">
-                        Sabpaisa.in
-                    </div>
-                    </div>
-                </div>
-                </div>
-            </div>  
-  )
-}
+    <div className="container-fluid bg-info">
+      <div className="row ">
+        <div className="col-sm-6 mx-auto">
+          <div className="card ">
+            <div className="card-header text-center">Forget Password</div>
+            <div className="card-body">
+              <h5 className="card-title">Please Enter the detatils. </h5>
+              <Formik
+                initialValues={initialValues}
+                validationSchema={validationSchema}
+                onSubmit={(values, { resetForm }) => {
+                  handleSubmit(values);
+                  console.log(values);
+                  resetForm();
+                }}
+              >
+                {({ resetForm }) => (
+                  <>
+                    <Form>
+                      <div className="form-group">
+                        <label htmlFor="exampleInputEmail1">
+                          Email address
+                        </label>
+                        <Field
+                          name="Email"
+                          type="email"
+                          className="form-control"
+                          id="exampleInputEmail1"
+                          aria-describedby="emailHelp"
+                          placeholder="Enter email"
+                        />
+                        {
+                          <ErrorMessage name="Email">
+                            {(msg) => (
+                              <div
+                                className="abhitest"
+                                style={{
+                                  color: "red",
+                                  position: "absolute",
+                                  zIndex: " 999",
+                                  marginTop: "15px",
+                                }}
+                              >
+                                {msg}
+                              </div>
+                            )}
+                          </ErrorMessage>
+                        }
+                        <small id="emailHelp" className="form-text text-muted">
+                          We'll never share your email with anyone else.
+                        </small>
+                      </div>
+                      <button
+                        type="submit"
+                        className="btn btn-primary"
+                        onClick={() => props.props("a2")}
+                      >
+                        Submit
+                      </button>
+                    </Form>
+                  </>
+                )}
+              </Formik>
+              <p className="card-text" style={{ display: "none" }}>
+                With supporting text below as a natural lead-in to additional
+                content.
+              </p>
+            </div>
+            <div className="card-footer text-muted text-center">
+              Sabpaisa.in
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default EnterUserID
+export default EnterUserID;
--- src/components/forgetpassword/ForgetPassword.js
+++ src/components/forgetpassword/ForgetPassword.js
@@ -1,53 +1,39 @@
-
-import React, { useState } from 'react'
-import HeaderPage from '../login/HeaderPage';
-import EnterUserID from './EnterUserID';
-import ResetPassword from './ResetPassword';
-import VerifyEmailPhone from './VerifyEmailPhone';
-import ThanksCard  from './ThanksCard'
-
+import React, { useState } from "react";
+import HeaderPage from "../login/HeaderPage";
+import EnterUserID from "./EnterUserID";
+import ResetPassword from "./ResetPassword";
+import VerifyEmailPhone from "./VerifyEmailPhone";
+import ThanksCard from "./ThanksCard";
 
 const ForgetPassword = () => {
-    const [showCard, setShowCard] = useState('a1')
+  const [showCard, setShowCard] = useState("a1");
 
-    const handleFormSubmit=(currentCard)=>{
-        setShowCard(currentCard);
-        console.log("currentCard",currentCard);
-    }
+  const handleFormSubmit = (currentCard) => {
+    setShowCard(currentCard);
+    console.log("currentCard", currentCard);
+  };
 
-    return (
-        <React.Fragment>
-            <HeaderPage/>
+  return (
+    <React.Fragment>
+      <HeaderPage />
 
-            {/* enter user id  */}
-            {showCard==='a1'? 
-            <EnterUserID props={handleFormSubmit} />
-            : <></>
-             }
-            
+      {/* enter user id  */}
+      {showCard === "a1" ? <EnterUserID props={handleFormSubmit} /> : <></>}
 
-            {/* enter received otp code */}
-            {showCard==='a2'? 
-            <VerifyEmailPhone props={handleFormSubmit}/>
-            : <></>
-             }
-            
-            {/* reset password */}
-             {showCard==='a3'? 
-             <ResetPassword props={handleFormSubmit}/>
-            : <></>
-             }
-            
-                {/* reset password */}
-             {showCard==='a4'? 
-             <ThanksCard props={handleFormSubmit}/>
-            : <></>
-             }
-        
+      {/* enter received otp code */}
+      {showCard === "a2" ? (
+        <VerifyEmailPhone props={handleFormSubmit} />
+      ) : (
+        <></>
+      )}
 
-        </React.Fragment>
-    )
-}
+      {/* reset password */}
+      {showCard === "a3" ? <ResetPassword props={handleFormSubmit} /> : <></>}
 
+      {/* reset password */}
+      {showCard === "a4" ? <ThanksCard props={handleFormSubmit} /> : <></>}
+    </React.Fragment>
+  );
+};
 
 export default ForgetPassword;
