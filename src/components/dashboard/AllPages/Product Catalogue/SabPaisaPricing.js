41f6b63747da856375f6b728e8f7594c7c99c5e4
BHUVAN-CHANDRA-JOSHI <bhuvanjoshi923@gmail.com>
2022-09-13 13:56:28 +0530

    Made SabPaisa Pricing UI

--- src/components/dashboard/AllPages/Product Catalogue/SabPaisaPricing.js
+++ src/components/dashboard/AllPages/Product Catalogue/SabPaisaPricing.js
@@ -1,21 +1,150 @@
 import React from 'react'
 import NavBar from '../../NavBar/NavBar'
+// import numbersimage from "../.../assets/images/numbersimage.png"
+import numbersimage from "../../../../assets/images/numbersimage.png"
+import Background from "../../../../assets/images/Background.png"
+import EnterPrice from "../../../../assets/images/EnterPrice.png"
+import Rectangle from "../../../../assets/images/Rectangle.png"
+import enterPriceImages from "../../../../assets/images/enterPriceImages.png"
+// import Line1 from "../../../../../assets/images/Line1.png"
 
 const SabPaisaPricing = () => {
   return (
     <section className="ant-layout">
-    <div>
-      <NavBar />
-      {/*  <div className="notification-bar"><span style="margin-right: 10px;">Please upload the documents<span className="btn">Upload Here</span></span></div>*/}
-    </div>
-    <main className="gx-layout-content ant-layout-content">
+      <div>
+        <NavBar />
+        {/*  <div className="notification-bar"><span style="margin-right: 10px;">Please upload the documents<span className="btn">Upload Here</span></span></div>*/}
+      </div>
+      <main className="gx-layout-content ant-layout-content">
+
+        <div>
+          <h1 className="text-center headingpricing">SabPaisa Pricing</h1>
+          <h3 className="forbasicparacss">We offer a very competitive pricing to match your business needs. Sign Up now to get started.</h3>
+        </div>
+
+        <div class="container mb-10">
+          <div class="row">
+            <div class="col-sm">
+              <div class="card heightcards">
+
+                <div class="card-body">
+                  <div class="row">
+                    <div class="col-sm">
+                      <h1 class="card-title cardoneheadingcss pb-3">Freelancer</h1>
+                    </div>
+                  </div>
+
+                  <img
+                    src={numbersimage}
+                    className="forimagespricing pb-5"
+                    alt="SabPaisa"
+                    title="SabPaisa"
+                  />
+                  <h3 className='paragraphcsss'>per user, per month</h3>
+                  <div class="container">
+                    <img
+                      src={Background}
+                      className="figmacssforchooseplan"
+                      alt="SabPaisa"
+                      title="SabPaisa"
+
+                    />
+                    <div class="centered">Choose Plan</div>
+                  </div>
+                </div>
+
+
+                <h2 className='featurespricing'>FEATURES INCLUDING</h2>
+
+                <ul className="forparacss">
+                  <li className="firstli">Chat Support</li>
+                  <li className="secondli">Complete online</li>
+                  <li className="thirdli">400+ Templates</li>
+                  <li className="forthli">Calendar View</li>
+                  <li className="fifthli">24/7 Support</li>
+                </ul>
+
+
+              </div>
+            </div>
+            <div class="col-sm">
+              <div class="card heightcards" style={{ background: "#012167", border: "1px solid #DBDBDB", boxShadow: "0px 4px 5px 1px rgb(193 193 193 / 35%)", borderRadius: "4px" }}>
+
 
-<h1>Hello</h1>
+                <div class="card-body">
+                  <div class="row">
+                    <div class="col-sm">
+                      <h1 class="card-title forsecondcardfigma text-white  pb-4">Enterprise</h1>
+                    </div>
+                  </div>
+                  <img
+                    src={EnterPrice}
+                    className="forimagesEnterpricing mb-4"
+                    alt="SabPaisa"
+                    title="SabPaisa"
+                  />
+                  <h3 className='graphcssforenterprice'>per user, per month</h3>
+                  <div class="container">
+                    <img
+                      src={Rectangle}
+                      className="figmacssforEnterpricechooseplan"
+                      alt="SabPaisa"
+                      title="SabPaisa"
 
-        
-    
+                    />
+                    <div class="centered text-white">Choose Plan</div>
+                  </div>
+                </div>
+                <h2 className='featurespricingforEnterpricing'>FEATURES INCLUDING</h2>
+                <ul className="forEnterPriceparaGcss">
+                  <li className="firstli">Chat Support</li>
+                  <li className="secondli">Complete online</li>
+                  <li className="thirdli">400+ Templates</li>
+                  <li className="forthli">Calendar View</li>
+                  <li className="fifthli">24/7 Support</li>
+                </ul>
+              </div>
+            </div>
+            <div class="col-sm">
+              <div class="card heightcards">
+                                <div class="card-body">
+                                <div class="row">
+                    <div class="col-sm">
+                      <h1 class="card-title pb-4 forThirdscardfigma">Business</h1>
+                    </div>
+                   </div>
+                   <img
+                    src={enterPriceImages}
+                    className="cssforbusinesscard mb-4"
+                    alt="SabPaisa"
+                    title="SabPaisa"
+                  />
+                   <h3 className='parabusinesscss'>per user, per month</h3>
+                   <div class="container">
+                    <img
+                      src={Background}
+                      className="figmacssforchooseplan"
+                      alt="SabPaisa"
+                      title="SabPaisa"
 
+                    />
+                    <div class="centered">Choose Plan</div>
+                  </div>
+                  </div>
+                  <h2 className='featuresIncludingforbusiness'>FEATURES INCLUDING</h2>
+                  <ul className="Businessoredrcssforcard">
+                  <li className="firstli">Chat Support</li>
+                  <li className="secondli">Complete online</li>
+                  <li className="thirdli">400+ Templates</li>
+                  <li className="forthli">Calendar View</li>
+                  <li className="fifthli">24/7 Support</li>
+                </ul>
+                  
 
+              </div>
+            </div>
+          </div>
+        </div>
 
 
 
@@ -76,11 +205,16 @@ const SabPaisaPricing = () => {
 
 
 
-    {/*  <=========== Old Product Catalogue ===========> */}
 
 
 
-    {/* <div className="gx-main-content-wrapper">
+
+
+        {/*  <=========== Old Product Catalogue ===========> */}
+
+
+
+        {/* <div className="gx-main-content-wrapper">
           <div className="right_layout my_account_wrapper right_side_heading">
             <h1 className="m-b-sm gx-float-left">Product Catalogue</h1>
           </div>
@@ -88,14 +222,14 @@ const SabPaisaPricing = () => {
             className="features8 cid-sg6XYTl25a flleft"
             id="features08-3-"
           > */}
-            <div className="container-fluid">
-              <div className="row">
+        <div className="container-fluid">
+          <div className="row">
 
-                {/* {subscriptionPlanData.length <= 0 ? (
+            {/* {subscriptionPlanData.length <= 0 ? (
                   <h3>Loading...</h3>
                 ) : ( */}
-                  {/* subscriptionPlanData.map((s, i) => ( */}
-                    {/* <div className="col col-lg-5 ">
+            {/* subscriptionPlanData.map((s, i) => ( */}
+            {/* <div className="col col-lg-5 ">
                       <div className="card">
                         <div className="card-body">
                           <h5 className="card-title font-weight-bold h3">
@@ -145,14 +279,14 @@ const SabPaisaPricing = () => {
 
 
 
-                            {/* <button
+            {/* <button
                                className=" btn bttn bttnbackgroundkyc collapsed"
                                 type="button"
                               >
                                 Subscribe
                               </button> */}
 
-                          {/* </p>
+            {/* </p>
                           <div
                             className="collapse"
                             id={`collapseExample${s.applicationId}`}
@@ -166,18 +300,18 @@ const SabPaisaPricing = () => {
                         <div className="container" />
                       </div>
                     </div> */}
-                  {/* )) */}
-                {/* )} */}
-              {/* </div>
+            {/* )) */}
+            {/* )} */}
+            {/* </div>
             </div>
           </section> */}
-              {/*  <=========== Old Product Catalogue ===========> */}
-        </div>
+            {/*  <=========== Old Product Catalogue ===========> */}
+          </div>
         </div>
 
-    </main>
-  </section>
-);
+      </main>
+    </section>
+  );
 };
 
 
