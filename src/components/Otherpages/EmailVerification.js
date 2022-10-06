1b24236f5a97393f053c8bdcc4fbca0394584e55
harrisfazal <harrisfazal@ymail.com>
2022-10-06 17:51:16 +0530

    Auth Instance added in Email Verify API

--- src/components/Otherpages/EmailVerification.js
+++ src/components/Otherpages/EmailVerification.js
@@ -3,7 +3,7 @@ import { Link ,useParams} from 'react-router-dom';
 import sabpaisalogo from '../../assets/images/sabpaisa-logo-white.png';
 import axios from 'axios';
 import API_URL from '../../config';
-import { axiosInstance } from '../../utilities/axiosInstance';
+import { axiosInstance,axiosInstanceAuth } from '../../utilities/axiosInstance';
 
 
 const EmailVerification = () => {
@@ -13,7 +13,7 @@ const EmailVerification = () => {
 
 
   useEffect(() => {
-    axiosInstance.put(`${API_URL.EMAIL_VERIFY}${loginId}`)
+    axiosInstanceAuth.put(`${API_URL.EMAIL_VERIFY}${loginId}`)
     .then((response) => {
       setData(response);
     })
