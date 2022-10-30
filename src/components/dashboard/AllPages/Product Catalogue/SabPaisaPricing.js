6c23ccf97ff0f15e2a5d8970e04ae3fd886e7e88
harrisfazal <harrisfazal@ymail.com>
2022-10-30 22:42:12 +0530

    fonts

--- src/components/dashboard/AllPages/Product Catalogue/SabPaisaPricing.js
+++ src/components/dashboard/AllPages/Product Catalogue/SabPaisaPricing.js
@@ -1,13 +1,13 @@
-import React, { useState, useEffect } from "react";
-import NavBar from "../../NavBar/NavBar";
-import rafiki from "../../../../assets/images/rafiki.png";
-import { productSubscribeState } from "../../../../slices/dashboardSlice";
-import { useDispatch, useSelector } from "react-redux";
-import API_URL from "../../../../config";
-import { axiosInstanceAuth } from "../../../../utilities/axiosInstance";
-import "./product.css";
-import toastConfig from "../../../../utilities/toastTypes";
-import { useParams } from "react-router-dom";
+import React, { useState, useEffect } from 'react'
+import NavBar from '../../NavBar/NavBar'
+import rafiki from "../../../../assets/images/rafiki.png"
+import { productSubscribeState } from '../../../../slices/dashboardSlice'
+import { useDispatch, useSelector } from 'react-redux'
+import API_URL from '../../../../config';
+import { axiosInstanceAuth } from '../../../../utilities/axiosInstance';
+import "./product.css"
+import toastConfig from '../../../../utilities/toastTypes';
+import { useParams } from 'react-router-dom'
 
 const SabPaisaPricing = () => {
   // const { dashboard } = useSelector((state) => state);
@@ -18,25 +18,27 @@ const SabPaisaPricing = () => {
   // const [serviceId, setServiceId] = useState(null)
   const dispatch = useDispatch();
   const clickHandler = (value) => {
-    dispatch(productSubscribeState(value));
-  };
+    dispatch(productSubscribeState(value))
+  }
   const { user } = useSelector((state) => state.auth);
-  console.log("user", user);
+  console.log("user",user)
   const { clientId, clientName } = user.clientMerchantDetailsList[0];
 
-  const param = useParams();
+  const param = useParams()
+
 
   useEffect(() => {
     // console.log("parma",param);
     const id = param?.id;
-    const name = param;
+    const name = param
     // console.log("this is params : ", name?.name)
-    let url = API_URL.PRODUCT_SUB_DETAILS + "/" + id;
+    let url = API_URL.PRODUCT_SUB_DETAILS + "/" + id
     axiosInstanceAuth
-      .get(url)
+      .get(url
+      )
       .then((resp) => {
         const data = resp.data.ProductDetail;
-        setSpinner(false);
+        setSpinner(false)
 
         setProductDetails(data);
         // productDetails.map((product)=>const arr = product.plan_description.split(","))
@@ -48,26 +50,26 @@ const SabPaisaPricing = () => {
       .catch((err) => console.log(err));
   }, [param]);
 
+
+
+
   const handleClick = async (plan_id, plan_name) => {
     const postData = {
       clientId: clientId,
-      applicationName: param?.name,
+      applicationName:param?.name,
       planId: plan_id,
       planName: plan_name,
       applicationId: param?.id,
     };
-    console.log("postData", postData);
+    console.log("postData",postData)
 
-    const res = await axiosInstanceAuth.post(
-      API_URL.SUBSCRIBE_FETCHAPPAND_PLAN,
-      postData
-    );
+    const res = await axiosInstanceAuth.post(API_URL.SUBSCRIBE_FETCHAPPAND_PLAN, postData);
     if (res.status === 200) {
-      toastConfig.successToast(res.data.message);
+      toastConfig.successToast(res.data.message)
     } else {
-      toastConfig.errorToast("something went wrong");
+      toastConfig.errorToast("something went wrong")
     }
-  };
+  }
 
   // const handleClick = (plan_id) => {
   //   const data = {
@@ -85,97 +87,62 @@ const SabPaisaPricing = () => {
         <NavBar />
         {/*  <div className="notification-bar"><span style="margin-right: 10px;">Please upload the documents<span className="btn">Upload Here</span></span></div>*/}
       </div>
-      <main className="gx-layout-content ant-layout-content Satoshi-Medium">
+      <main className="gx-layout-content ant-layout-content">
+
         <div>
           <h1 className="text-center headingpricing">SabPaisa Pricing</h1>
-          <h3 className="forbasicparacss">
-            We offer a very competitive pricing to match your business needs.
-            Sign Up now to get started.
-          </h3>
+          <h3 className="forbasicparacss">We offer a very competitive pricing to match your business needs. Sign Up now to get started.</h3>
         </div>
         {/* <button type="button" onClick={clickHandler}>check</button> */}
         <div class="container mb-10">
           <div class="row">
+          
+
             <div class="col-sm">
-              <div
-                class="card heightcards"
-                style={{ height: "620px", width: "300px" }}
-              >
+              <div class="card heightcards" style={{ height: "620px", width: "300px" }}>
+
                 <div class="card-body">
                   <div class="row mb-5">
-                    <div className="col-lg-12 text-center">
-                      {spinner && (
-                        <span
-                          className="spinner-border borders"
-                          role="status"
-                        ></span>
-                      )}
-                      <h1 class="card-title cardoneheadingcss pb-3">
-                        {productDetails[0]?.plan_name}
-                      </h1>
-                      <p className="text-center bold-font mb-1">
-                        {productDetails[0]?.plan_price}
-                      </p>
-                      <h3 className="paragraphcsss text-center">
-                        {productDetails[0]?.plan_type}
-                      </h3>
-                      <button
-                        type="button"
-                        className=" font-weight-bold btn choosePlan-1 btn-lg"
-                        data-toggle="modal"
-                        data-target="#exampleModal"
-                        onClick={() =>
-                          handleClick(
-                            productDetails[0].plan_id,
-                            productDetails[0].plan_name
-                          )
-                        }
-                      >
-                        Choose Plan
-                      </button>
+                    <div className='col-lg-12 text-center'>
+                    {spinner && (
+            <span
+              className="spinner-border borders"
+              role="status"
+            ></span>
+          )}
+                      <h1 class="card-title cardoneheadingcss pb-3">{productDetails[0]?.plan_name}</h1>
+                      <p className='text-center bold-font mb-1'>{productDetails[0]?.plan_price}</p>
+                      <h3 className='paragraphcsss text-center'>{productDetails[0]?.plan_type}</h3>
+                      <button type="button" className=" font-weight-bold btn choosePlan-1 btn-lg" data-toggle="modal" data-target="#exampleModal" onClick={() => handleClick(productDetails[0].plan_id, productDetails[0].plan_name)}  >Choose Plan</button>
                       {/* <button type="button" className=" font-weight-bold btn choosePlan-1 btn-lg" data-toggle="modal" data-target="#exampleModal"><span style={{ color: "#1465FA" }} onClick={() => handleClick(productDetails[0].plan_id)}>Choose Plan</span></button> */}
                     </div>
+
+
                   </div>
+
+
 
                   {/* <button onClick={() => clickHandler(true)} type="button" className="figmacssforchooseplan mt-2" data-toggle="modal" data-target="#exampleModal">
                     <h5 className="chooseplanheadingcss">Choose Plan</h5>
                   </button> */}
 
-                  <div
-                    class="modal fade"
-                    id="exampleModal"
-                    tabindex="-1"
-                    role="dialog"
-                    aria-labelledby="exampleModalLabel"
-                    aria-hidden="true"
-                  >
-                    <div
-                      class="modal-dialog"
-                      style={{ maxWidth: 480 }}
-                      role="document"
-                    >
+                  <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog" style={{ maxWidth: 480 }} role="document">
                       <div class="modal-content">
-                        <div class="modal-header modal-header-fignma">
-                          <button
-                            type="button"
-                            class="close"
-                            data-dismiss="modal"
-                            aria-label="Close"
-                            // onClick={() => clickHandler(false)}
+                        <div class="modal-header modal-header-fignma" >
+                          <button type="button" class="close" data-dismiss="modal" aria-label="Close"
+                          // onClick={() => clickHandler(false)}
                           >
                             <span aria-hidden="true">&times;</span>
                           </button>
                         </div>
                         <div class="modal-body">
-                          <h2 className="subscribingproduct mb-0">
-                            Thank You For Subscribing
-                          </h2>
+                          <h2 className="subscribingproduct mb-0" >Thank You For Subscribing</h2>
 
                           <div class="text-center">
-                            <h2 className="manshacss">
-                              Mansha(bot) will now help you integrate your
-                              website with our payment product.
+                            <h2 className="manshacss">Mansha(bot) will now help you integrate your website with our payment product.
                             </h2>
+
                           </div>
                           <div class="row">
                             <div class="col-lg-12 text-center">
@@ -191,20 +158,16 @@ const SabPaisaPricing = () => {
                         </div>
                         <div class="modal-footer m-0 p-2">
                           <div className="col-lg-12 text-center">
-                            <button
-                              type="button"
-                              class="ColrsforredirectProdct text-white m-0"
+                            <button type="button" class="ColrsforredirectProdct text-white m-0"
                               // onClick={() => clickHandler(false)}
-                              data-dismiss="modal"
-                            >
-                              Close
-                            </button>
+                              data-dismiss="modal">Close</button>
                           </div>
+
                         </div>
                       </div>
                     </div>
                   </div>
-                  <h2 className="featurespricing">FEATURES INCLUDING</h2>
+                  <h2 className='featurespricing'>FEATURES INCLUDING</h2>
 
                   {/* <ul className="forparacss">
   <li className="firstli">AI Chat Support</li>
@@ -213,121 +176,126 @@ const SabPaisaPricing = () => {
   <li className="forthli">Advanced Analytics</li>
   <li className="fifthli">Dashboard training</li>
 </ul> */}
-                  <div className="text-center">
-                    {productDetails[0]?.plan_description
-                      .split(",")
-                      .map((details, i) => (
-                        <p className="firstli1 mb-2">{details}</p>
-                      ))}
+                  <div className='text-center'>
+                    {productDetails[0]?.plan_description.split(',').map((details, i) => <p className="firstli1 mb-2">{details}</p>)}
                   </div>
                 </div>
+
+
               </div>
             </div>
             <div class="col-sm">
-              <div
-                class="card heightcards"
-                style={{
-                  background: "#012167",
-                  border: "1px solid #DBDBDB",
-                  boxShadow: "0px 4px 5px 1px rgb(193 193 193 / 35%)",
-                  borderRadius: "4px",
-                  height: "620px",
-                  width: "300px",
-                }}
-              >
+              <div class="card heightcards" style={{ background: "#012167", border: "1px solid #DBDBDB", boxShadow: "0px 4px 5px 1px rgb(193 193 193 / 35%)", borderRadius: "4px", height: "620px", width: "300px" }}>
                 <div class="card-body">
                   <div class="row mb-5">
-                    <div className="col-lg-12 text-center text-white">
-                      <h1 class="card-title cardoneheadingcss2 pb-3">
-                        {productDetails[1]?.plan_name}
-                      </h1>
-                      <p className="text-center text-white bold-font mb-1">
-                        {productDetails[1]?.plan_price}
-                      </p>
-                      <h3 className="paragraphcsss text-white text-center">
-                        {productDetails[1]?.plan_type}
-                      </h3>
-                      <button
-                        type="button"
-                        className="btn choosePlan-2 btn-primary btn-lg font-weight-bold"
-                        data-toggle="modal"
-                        data-target="#exampleModal"
-                        onClick={() =>
-                          handleClick(
-                            productDetails[1].plan_id,
-                            productDetails[1].plan_name
-                          )
-                        }
-                      >
-                        Choose Plan
-                      </button>
+                    <div className='col-lg-12 text-center text-white'>
+                      <h1 class="card-title cardoneheadingcss2 pb-3">{productDetails[1]?.plan_name}</h1>
+                      <p className='text-center text-white bold-font mb-1'>{productDetails[1]?.plan_price}</p>
+                      <h3 className='paragraphcsss text-white text-center'>{productDetails[1]?.plan_type}</h3>
+                      <button type="button" className="btn choosePlan-2 btn-primary btn-lg font-weight-bold" data-toggle="modal" data-target="#exampleModal" onClick={() => handleClick(productDetails[1].plan_id, productDetails[1].plan_name)}  >Choose Plan</button>
                     </div>
                   </div>
 
-                  <h2 className="featurespricingforEnterpricing">
-                    FEATURES INCLUDING
-                  </h2>
-                  <div className="text-center text-white">
-                    {productDetails[1]?.plan_description
-                      .split(",")
-                      .map((details, i) => (
-                        <p className="firstli1 mb-2 text-white">{details}</p>
-                      ))}
+                  <h2 className='featurespricingforEnterpricing'>FEATURES INCLUDING</h2>
+                  <div className='text-center text-white'>
+                    {productDetails[1]?.plan_description.split(',').map((details, i) => <p className="firstli1 mb-2 text-white">{details}</p>)}
                   </div>
                 </div>
+
               </div>
             </div>
             <div class="col-sm">
-              <div
-                class="card heightcards"
-                style={{ height: "620px", width: "300px" }}
-              >
+              <div class="card heightcards" style={{ height: "620px", width: "300px" }}>
                 <div class="card-body">
+
                   <div class="row mb-5">
-                    <div className="col-lg-12 text-center">
-                      <h1 class="card-title cardoneheadingcss pb-3">
-                        {productDetails[2]?.plan_name}
-                      </h1>
+                    <div className='col-lg-12 text-center'>
+                      <h1 class="card-title cardoneheadingcss pb-3">{productDetails[2]?.plan_name}</h1>
                       {/* <p className='text-center bold-font mb-1'></p> */}
-                      <h3 className="bold-font text-center mb-1">
-                        {productDetails[2]?.plan_price}
-                      </h3>
-                      <h3 className="paragraphcsss text-center">
-                        {productDetails[2]?.plan_type}
-                      </h3>
-                      <button
-                        type="button"
-                        className="btn choosePlan-1  btn-lg font-weight-bold"
-                        data-toggle="modal"
-                        data-target="#exampleModal"
-                        onClick={() =>
-                          handleClick(
-                            productDetails[2].plan_id,
-                            productDetails[2].plan_name
-                          )
-                        }
-                      >
-                        Contact Sales
-                      </button>
+                      <h3 className='bold-font text-center mb-1'>{productDetails[2]?.plan_price}</h3>
+                      <h3 className='paragraphcsss text-center'>{productDetails[2]?.plan_type}</h3>
+                      <button type="button" className="btn choosePlan-1  btn-lg font-weight-bold" data-toggle="modal" data-target="#exampleModal" onClick={() => handleClick(productDetails[2].plan_id, productDetails[2].plan_name)}  >Contact Sales</button>
                     </div>
                   </div>
-                  <h2 className="featuresIncludingforbusiness">
-                    FEATURES INCLUDING
-                  </h2>
-                  <div className="text-center">
-                    {productDetails[2]?.plan_description
-                      .split(",")
-                      .map((details, i) => (
-                        <p className="firstli1 mb-2">{details}</p>
-                      ))}
+                  <h2 className='featuresIncludingforbusiness'>FEATURES INCLUDING</h2>
+                  <div className='text-center'>
+                    {productDetails[2]?.plan_description.split(',').map((details, i) => <p className="firstli1 mb-2">{details}</p>)}
                   </div>
                 </div>
+
+
               </div>
             </div>
           </div>
         </div>
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         {/*  <=========== Old Product Catalogue ===========> */}
+
+
 
         {/* <div className="gx-main-content-wrapper">
           <div className="right_layout my_account_wrapper right_side_heading">
@@ -339,6 +307,7 @@ const SabPaisaPricing = () => {
           > */}
         <div className="container-fluid">
           <div className="row">
+
             {/* {subscriptionPlanData.length <= 0 ? (
                   <h3>Loading...</h3>
                 ) : ( */}
@@ -391,6 +360,8 @@ const SabPaisaPricing = () => {
                               </div>
                             </div> */}
 
+
+
             {/* <button
                                className=" btn bttn bttnbackgroundkyc collapsed"
                                 type="button"
@@ -420,9 +391,12 @@ const SabPaisaPricing = () => {
             {/*  <=========== Old Product Catalogue ===========> */}
           </div>
         </div>
+
       </main>
     </section>
   );
 };
 
-export default SabPaisaPricing;
+
+
+export default SabPaisaPricing
