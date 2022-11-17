8198f3b05761f29527304edb1b1ae5e07ab8f33d
moneyverma <46442522+moneyverma@users.noreply.github.com>
2022-11-17 18:56:18 +0530

    fix repeat answer in chat bot

--- src/components/chatbot/Components/AnswerComponent.jsx
+++ src/components/chatbot/Components/AnswerComponent.jsx
@@ -11,7 +11,7 @@ const AnswerComponent = (props)=>{
 
 useEffect(() => {
     // console.log(props?.propscons
-    {console.log("check4 ANswer", props.props.previousStep.value.data)}
+    // {console.log("check4 ANswer", props.props.previousStep.value.data)}
     props?.props?.triggerNextStep({trigger: 7})
 //   return () => {
 //     second
@@ -29,7 +29,10 @@ useEffect(() => {
             </div>
             <div className="answer">
 
-                <p>{props.props.previousStep.value.data.answer.Answer}  </p>
+                <p>
+                {props.props.previousStep.value.data.answer.Answer}
+                {props?.props?.previousStep?.value?.data?.answer?.AnswerID===2? <a href="https://sabpaisa.in/" target="_blank" rel="noreferrer" alt="sabpaisa" className="web_url">https://sabpaisa.in/</a> : ""} 
+                </p>
 
             </div>
         </div>
