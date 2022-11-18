de5b5eabb643083a32df133c04b3a7cf646cd00d
harrisfazal <harrisfazal@ymail.com>
2022-11-18 14:45:35 +0530

    Changes in Regex KYC form

--- src/_components/formik/ValidationRegex.js
+++ src/_components/formik/ValidationRegex.js
@@ -3,6 +3,7 @@ export const Regex = {
     acceptAlphabet : /^[aA-zZ\s]+$/ ,
     acceptNumber : /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/,
     acceptAlphaNumeric : /^[a-zA-Z0-9\s]+$/, 
+    alphaBetwithhyphon:/[a-zA-Z._^%$#!~@,-]+/,
     digit : /^[0-9]+$/, 
     address: /^[a-zA-Z0-9\s,.'-]{3,}$/
 }
@@ -12,6 +13,7 @@ export const RegexMsg = {
     acceptNumber : "Only Numbers are allowed for this field " ,
     acceptAlphaNumeric : "Only alphanumeric are allowed for this field",
     digit : "Only Digit are allowed for this field",
+    alphaBetwithhyphon:"Only alphabets and '-' are allowed for this field",
     address : "Address is not in valid format, Special Characters are not allowed (#,$,@ etc)"
 }
 
