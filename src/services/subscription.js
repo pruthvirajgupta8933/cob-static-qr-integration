393dde8bde7cdc214ea349359e2d1008cfbb2096
moneyverma <46442522+moneyverma@users.noreply.github.com>
2022-11-18 12:19:25 +0530

    remove and comment unwanted code

--- src/services/subscription.js
+++ src/services/subscription.js
@@ -1,36 +1,36 @@
 import axios from "axios";
 import { axiosInstance } from "../utilities/axiosInstance";
 
-const SUBSCRIPTION_URL = "https://spl.sabpaisa.in/client-subscription-service/";
+// const SUBSCRIPTION_URL = "https://spl.sabpaisa.in/client-subscription-service/";
 
-const subscriptionPlan = () => {
-  return axiosInstance.get(SUBSCRIPTION_URL + "fetchAppAndPlan")
-  .then((response) => {
-    if (response.data) {
-      localStorage.setItem("subscriptionplan", JSON.stringify(response.data));
-    } else {
-      localStorage.setItem("subscriptionplan", JSON.stringify(response.data));
-    }
-    return response.data;
-  });
-};
+// const subscriptionPlan = () => {
+//   return axiosInstance.get(SUBSCRIPTION_URL + "fetchAppAndPlan")
+//   .then((response) => {
+//     if (response.data) {
+//       localStorage.setItem("subscriptionplan", JSON.stringify(response.data));
+//     } else {
+//       localStorage.setItem("subscriptionplan", JSON.stringify(response.data));
+//     }
+//     return response.data;
+//   });
+// };
 
-const subscriptionChargesDetail = () => {
-    return axiosInstance.post(SUBSCRIPTION_URL + "subscribe")
-    .then((response) => {
-      if (response.data) {
-        localStorage.setItem("subscriptionchargesdetail", JSON.stringify(response.data));
-      } else {
-        localStorage.setItem("subscriptionchargesdetail", JSON.stringify(response.data));
-      }
+// const subscriptionChargesDetail = () => {
+//     return axiosInstance.post(SUBSCRIPTION_URL + "subscribe")
+//     .then((response) => {
+//       if (response.data) {
+//         localStorage.setItem("subscriptionchargesdetail", JSON.stringify(response.data));
+//       } else {
+//         localStorage.setItem("subscriptionchargesdetail", JSON.stringify(response.data));
+//       }
   
-      return response.data;
-    });
-  };
+//       return response.data;
+//     });
+//   };
 
 const subscriptionService = {
-    subscriptionPlan,
-    subscriptionChargesDetail,
+    // subscriptionPlan,
+    // subscriptionChargesDetail,
 };
 
 export default subscriptionService;
