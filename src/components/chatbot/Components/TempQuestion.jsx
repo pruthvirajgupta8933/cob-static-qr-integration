9a92f75aaebda925001f0ddbf206b0657f2cccdc
unknown <mohd.kashif@sabpaisa.in>
2022-12-07 15:56:13 +0530

    Added new steps and enabled input box

--- src/components/chatbot/Components/TempQuestion.jsx
+++ src/components/chatbot/Components/TempQuestion.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import axios from 'axios'
 
@@ -10,54 +10,38 @@ import axios from 'axios'
 const TempQuestion = (props)=>{
 
     const {steps} = props
-    const personalDetail = {
-        // name : steps[3].value.name,
-        // mobile: steps[3].value.contactNumber,
-        // email: steps[3].value.email,
-        // CategoryId: steps[3].value.CategoryId
-        name : "ManshaAdmin",
-        mobile: 1111111111,
-        email: 'abc@sabpaisa.in',
-        CategoryId: 1
-    }
-    const [Detail, setDetails]= useState(personalDetail)
-    const SetValues = (e)=>{
-        const { name, value } = e.target;
-        setDetails(prevState => ({
-            ...prevState,
-            [name]: value
-        })) 
-    }
-   
+    
+    // const [Detail, setDetails]= useState(personalDetail)
+    // const SetValues = (e)=>{
+    //     const { name, value } = e.target;
+    //     setDetails(prevState => ({
+    //         ...prevState,
+    //         [name]: value
+    //     })) 
+    // }
     const SubmitValues = () =>{
-        axios.post("https://chatbotadmin.sabpaisa.in/chatbot/tempquestion", Detail,{
+        const personalDetail = {
+            name : steps[16].value,
+            mobile: steps[18].value,
+            email: steps[20].value,
+            Question: steps[22].value,
+            CategoryId: 1
+        }
+        axios.post("https://chatbotadmin.sabpaisa.in/chatbot/tempquestion", personalDetail,{
             headers: {
                 'authorization': 'ddbf42c6-078a-404f-b4ed-d47faaa52bbf'
             }
         }).then((res)=>{
-            props.triggerNextStep({ trigger: 64 })
+            props.triggerNextStep({ trigger: 7 })
         }).catch((err)=>{
             props.triggerNextStep({ trigger: 500 })
         })  
     }
-
+    useEffect(()=>{
+        SubmitValues()
+    }, [])
     return(
-        <>
-        <div>
-        <div>
-            <div item container alignItems="center" justifyContent="center" spacing={2}>
-                <div item xs={12}>
-                    <input style={{"marginTop": "10px"}} id="outlined-multiline-static" label="Question" name="Question" multiline rows={3} onChange={SetValues}/>
-                </div>
-            </div>
-            <div item container alignItems="center" justifyContent="center" spacing={2}>
-                <div item xs={4}>
-                        <button style={{"marginTop": "5px"}} className="submitbutton" variant="contained" onClick={SubmitValues}>Submit</button>
-                </div>
-            </div>
-        </div>
-        </div>
-        </>
+        <p>your Query has been submitted</p>
     )
 }
 
