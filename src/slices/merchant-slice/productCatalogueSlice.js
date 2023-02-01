387cc0552f6c000f8d3136e15a46184e0fd1c820
moneyverma <46442522+moneyverma@users.noreply.github.com>
2023-02-01 19:15:43 +0530

    working to integrate PG for subscription

---
+++ src/slices/merchant-slice/productCatalogueSlice.js
@@ -0,0 +1,52 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { fetchSubscribedPlan } from "../../services/merchant-service/prouduct-catalogue.service";
+import { setMessage } from "../message";
+
+
+const initialState = { 
+  unPaidSubscribedPlan: [], 
+  SubscribedPlanData:[],
+  isLoading:false  }
+
+export const merchantSubscribedPlanData = createAsyncThunk(
+    "productCatalogue/merchantSubscribedPlanData",
+    async (object, thunkAPI) => {
+      try {
+        const data = await fetchSubscribedPlan(object);
+        return { data : data };
+      } catch (error) {
+        const message =
+          (error.response &&
+            error.response.data &&
+            error.response.data.message) ||
+          error.message ||
+          error.toString();
+        thunkAPI.dispatch(setMessage(message));
+        return thunkAPI.rejectWithValue();
+      }
+    }
+  );
+
+
+const productCatalogueSlice = createSlice({
+  name: "productCatalogue",
+  initialState,
+  extraReducers: {
+    [merchantSubscribedPlanData.pending]: (state) => {
+      state.isLoading = true
+      state.SubscribedPlanData = [];
+    },
+    [merchantSubscribedPlanData.fulfilled]: (state, action) => {
+      console.log(action.payload)
+      state.isLoading = false
+      state.SubscribedPlanData = action.payload?.data?.data?.data;
+    },
+    [merchantSubscribedPlanData.rejected]: (state) => {
+      state.isLoading = false
+      state.SubscribedPlanData = [];
+    }
+  },
+});
+
+
+export default productCatalogueSlice.reducer;
