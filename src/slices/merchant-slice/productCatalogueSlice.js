a1464174f1922a7b670b30da9d18a6af2a3f8ea5
harrisfazal <harrisfazal@ymail.com>
2023-02-03 10:27:15 +0530

    Fine Tuning and UI fix

--- src/slices/merchant-slice/productCatalogueSlice.js
+++ src/slices/merchant-slice/productCatalogueSlice.js
@@ -37,7 +37,7 @@ const productCatalogueSlice = createSlice({
       state.SubscribedPlanData = [];
     },
     [merchantSubscribedPlanData.fulfilled]: (state, action) => {
-      console.log(action.payload)
+      // console.log(action.payload)
       state.isLoading = false
       state.SubscribedPlanData = action.payload?.data?.data?.data;
     },
