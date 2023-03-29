fc725e9938f963c9d9678b8ae5a6725f93d85e2d
ANAND shaw <anand.kumar@sabpaisa.in>
2023-03-29 19:17:06 +0530

    Mandate Registration

--- src/subscription_components/Create_Mandate/Mandate_Submission/mandateDetails/emandateSummary.js
+++ src/subscription_components/Create_Mandate/Mandate_Submission/mandateDetails/emandateSummary.js
@@ -1,17 +1,18 @@
 import React from "react";
 
-const EmandateSummary = ({updatedData}) => {
+const EmandateSummary = ({ updatedData }) => {
+  console.log("EEEEEEEEmandate Summary Object", updatedData);
 
   return (
     <div>
       <div class="container">
         <div class="row">
-          <div class="col-sm"><h4>Mandate Variant</h4><span><p className="text-secondary">Mandate</p></span></div>
-          <div class="col-sm"><h4>Name</h4><p className="text-secondary">{updatedData?.requestType}</p></div>
-          <div class="col-sm"><h4>Bank Name</h4><p className="text-secondary"></p></div>
+          <div class="col-sm"><h4>Mandate Variant</h4><span><p className="text-secondary"></p></span></div>
+          <div class="col-sm"><h4>Name</h4><p className="text-secondary">{updatedData?.payerName}</p></div>
+          <div class="col-sm"><h4>Bank Name</h4><p className="text-secondary"></p>{updatedData?.payerBank}</div>
         </div>
         <div>
-          
+
         </div>
       </div>
     </div>
--- src/subscription_components/Create_Mandate/Mandate_Submission/mandateDetails/mandateSummary.js
+++ src/subscription_components/Create_Mandate/Mandate_Submission/mandateDetails/mandateSummary.js
@@ -12,68 +12,53 @@ const MandateSummary = ({updatedData}) => {
         </div>
         <div class="col-sm">
           <h4>Name</h4>
-          <p className="text-secondary"></p>
+          <p className="text-secondary">{updatedData?.payerName}</p>
         </div>
         <div class="col-sm">
           <h4>Bank Name</h4>
-          <p className="text-secondary"></p>
+          <p className="text-secondary">{updatedData?.payerBank}</p>
         </div>
       </div>
 
       <div>
-        <div class="row">
-          <div class="col-sm">
-            <h4>Requested By</h4>
-            <span>
-              <p className="text-secondary"></p>
-            </span>
-          </div>
-          <div class="col-sm">
-            <h4>Mandate Variant</h4>
-            <p className="text-secondary"></p>
-          </div>
-          <div class="col-sm">
-            <h4>Mandate Purpose</h4>
-            <p className="text-secondary"></p>
-          </div>
-        </div>
+        
         <div class="row">
           <div class="col-sm">
             <h4>ECS Amount</h4>
             <span>
-              <p className="text-secondary"></p>
+              <p className="text-secondary">{updatedData?.emiamount}</p>
             </span>
           </div>
           <div class="col-sm">
             <h4>Frequency</h4>
-            <p className="text-secondary"></p>
+            <p className="text-secondary">{updatedData?.frequency}</p>
           </div>
           <div class="col-sm">
             <h4>Fixed/Maximum Amount</h4>
-            <p className="text-secondary"></p>
+            <p className="text-secondary">{updatedData?.mandateMaxAmount}</p>
           </div>
         </div>
         <div class="row">
           <div class="col-sm">
             <h4>Consumer Reference Number</h4>
             <span>
-              <p className="text-secondary"></p>
+              <p className="text-secondary">{updatedData?.consumerReferenceNumber}</p>
             </span>
           </div>
           <div class="col-sm">
             <h4>Scheme Reference Number</h4>
-            <p className="text-secondary"></p>
+            <p className="text-secondary">{updatedData?.schemeReferenceNumber}</p>
           </div>
           <div class="col-sm">
             <h4>Start date</h4>
-            <p className="text-secondary"></p>
+            <p className="text-secondary">{updatedData?.mandateStartDate}</p>
           </div>
         </div>
         <div class="row">
           <div class="col-sm">
             <h4>End date</h4>
             <span>
-              <p className="text-secondary"></p>
+              <p className="text-secondary">{updatedData?.mandateEndDate}</p>
             </span>
           </div>
           <div class="col-sm">
@@ -82,6 +67,18 @@ const MandateSummary = ({updatedData}) => {
           </div>
           <div class="col-sm">
             <h4>Request Type</h4>
+            <p className="text-secondary">{updatedData?.requestType}</p>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-sm">
+            <h4>Requested By</h4>
+            <span>
+              <p className="text-secondary"></p>
+            </span>
+          </div>
+          <div class="col-sm">
+            <h4>Mandate Purpose</h4>
             <p className="text-secondary"></p>
           </div>
         </div>
