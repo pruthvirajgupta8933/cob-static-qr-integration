b761262493da173b235e2077a687981f37799f86
moneyverma <46442522+moneyverma@users.noreply.github.com>
2023-05-09 12:29:51 +0530

    Display the new value and modify the UI - Txn Enquiry

--- src/_components/formik/components/Input.js
+++ src/_components/formik/components/Input.js
@@ -2,19 +2,17 @@ import React from "react";
 import { Field, ErrorMessage } from "formik";
 
 function Input(props) {
-  const { name, label, ...rest } = props;
+  const { name, label, lableClassName, ...rest } = props;
   // console.log("label",label);
   return (
     <React.Fragment>
-      {typeof label !== "undefined" ? (
-        <label htmlFor={name}> {label}</label>
-      ) : (
-        <></>
+      {typeof label !== "undefined" && (
+        <label htmlFor={name} className={lableClassName}> {label}</label>
       )}
+
       <Field name={name} {...rest} />
       <p className="text-danger">
-        {" "}
-        <ErrorMessage name={name} />{" "}
+        <ErrorMessage name={name} />
       </p>
     </React.Fragment>
   );
