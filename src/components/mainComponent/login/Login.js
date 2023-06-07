6ec638ba0d6d86f22430d0d7112fd76263c30e68
moneyverma <46442522+moneyverma@users.noreply.github.com>
2023-06-07 12:40:32 +0530

    Fixed the route url with query str

--- src/components/mainComponent/login/Login.js
+++ src/components/mainComponent/login/Login.js
@@ -125,6 +125,7 @@ function Login() {
             })
         }
     }
+    // const queryStringUrl = window.location.search;
 
 
     return (
@@ -237,7 +238,7 @@ function Login() {
                             </div>
                         
                             <div className="text-center mt-2">
-                                <p className={`${classes.sp_font_14}`}>Don’t have an account with SabPaisa?  <Link className="text-primary text-decoration-underline" to="/Registration">Sign Up</Link></p>
+                                <p className={`${classes.sp_font_14}`}>Don’t have an account with SabPaisa?  <Link className="text-primary text-decoration-underline" to={`/Registration/${queryString}`}>Sign Up</Link></p>
                             </div>
                         </div>
                         <div className="bd-highlight text-center sp-font-12"><p><a href="https://sabpaisa.in/term-conditions/" rel="noreferrer"  target="_blank">Terms &amp; Conditions </a> | <a href="https://sabpaisa.in/privacy-policy/" rel="noreferrer"  target="_blank">Privacy Policy</a></p></div>
