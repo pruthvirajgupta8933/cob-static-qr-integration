bba3718fc2edd3101308e6ac52700b3347905820
moneyverma <46442522+moneyverma@users.noreply.github.com>
2023-06-21 15:06:54 +0530

    Add permission - Allow approver permission to vinayak

--- src/utilities/permisson.js
+++ src/utilities/permisson.js
@@ -1,3 +1,3 @@
 // 10424, 10423 - staging approver login id
-// 1575, 9856, 9829 - production approver login id
-export const ALLOW_ROLE_AS_VERIFIER = [10424, 10423, 1575, 9856];  // for selected approver
+// 1575, 9856, 9829, 10830 - production approver login id
+export const ALLOW_ROLE_AS_VERIFIER = [10424, 10423, 1575, 9856, 10830];  // for selected approver
