38df4aa17515efa09089d814ec275489c050c3ee
moneyverma <46442522+moneyverma@users.noreply.github.com>
2024-04-29 12:16:30 +0530

    bug fix: web url regex validation/ logout if user session exporired

--- src/services/setupInterceptors.js
+++ src/services/setupInterceptors.js
@@ -10,6 +10,8 @@ const setup = async (store) => {
       const token = TokenService.getLocalAccessToken();
       if (token) {
         config.headers["Authorization"] = 'Bearer ' + token;
+      } else {
+        console.log("Token expired")
       }
       return config;
     },
@@ -19,48 +21,6 @@ const setup = async (store) => {
   );
 
 
-  //   axiosInstanceJWT.interceptors.response.use(
-  //     (response) => {
-  //       return response
-  //     },
-  //     (error) => {
-  //       return new Promise((resolve) => {
-  //         const originalRequest = error.config
-  //         const refreshToken = TokenService.getLocalrefreshToken()
-  //         // console.log("error.response", error.response)
-  //         // console.log("error.response.status", error.response.status)
-  //         // console.log("error.config", error.config)
-  //         // console.log("!error.config.__isRetryRequest", !error.config.__isRetryRequest)
-  //         // console.log("refreshToken", refreshToken)
-  //         if (error.response && error.response.status === 401 && error.config && !error.config.__isRetryRequest && refreshToken) {
-  //           // console.log("true conditions")
-  //           originalRequest._retry = true
-  //           const response = fetch(API_URL.BASE_URL_COB + "/auth-service/auth/refresh-token", {
-  //             method: 'POST',
-  //             headers: {
-  //               'Content-Type': 'application/json',
-  //             },
-  //             body: JSON.stringify({
-  //               refresh: refreshToken,
-  //             }),
-  //           })
-  //             .then((res) => res.json())
-  //             .then((res) => {
-
-  //               // localStorage.set(res.access, 'token')
-  //               TokenService.updateLocalAccessToken(res.access);
-  //               return axiosInstanceJWT(originalRequest)
-  //             }).catch(err => {
-  //               authService.logout();
-  //               window.location.reload();
-  //               console.log("catch err --------------logout----------")
-  //             })
-  //           resolve(response)
-  //         }
-  //         return Promise.reject(error)
-  //       })
-  //     },
-  //   )
 
 
 
