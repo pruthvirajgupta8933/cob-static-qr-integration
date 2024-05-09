1935a5777b8c0f4c5a4b1a7e48b05202fc308074
moneyverma <46442522+moneyverma@users.noreply.github.com>
2024-05-09 16:33:54 +0530

    remove console

--- src/services/dashboard.service.js
+++ src/services/dashboard.service.js
@@ -13,7 +13,6 @@ const successTxnSummary = (object) => {
 
 // Graph 
 const getTxnDataForGraph = (object) => {
-  console.log(object)
   return axiosInstance.post(API_URL.GET_TRANSACTION_DATA_CHART, object)
 }
 
