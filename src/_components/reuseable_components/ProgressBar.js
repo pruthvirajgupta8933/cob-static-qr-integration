06ff4cda4e730189b980c851658721ec5df7d588
moneyverma <46442522+moneyverma@users.noreply.github.com>
2024-05-17 15:27:55 +0530

    Feat: add loader on sidebar

--- src/_components/reuseable_components/ProgressBar.js
+++ src/_components/reuseable_components/ProgressBar.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
-function ProgressBar({}) {
+function ProgressBar() {
     return (
         <div className='col-lg-12' >
             <div className="text-center p-4 m-4">
-                <div className="spinner-border" style={{width: '2rem', height: '2rem'}} >
+                <div className="spinner-border" style={{ width: '2rem', height: '2rem' }} >
                     <span className="sr-only">Loading...</span>
                 </div>
             </div>
