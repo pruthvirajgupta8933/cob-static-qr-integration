6e8086e559cd8fba851e5656a720af8a2412f11b
moneyverma <46442522+moneyverma@users.noreply.github.com>
2024-06-28 12:41:01 +0530

    feat: Modify the export report API

--- src/components/ApproverNVerifier/MerchnatListExportToxl.js
+++ src/components/ApproverNVerifier/MerchnatListExportToxl.js
@@ -1,43 +1,48 @@
 /* eslint-disable array-callback-return */
-import React, { useState} from 'react'
+import React, { useState } from 'react'
 import { axiosInstanceJWT } from '../../utilities/axiosInstance';
 import API_URL from '../../config';
-import Blob from "blob";
+// import Blob from "blob";
 import toastConfig from '../../utilities/toastTypes';
 
 const MerchnatListExportToxl = (props) => {
   const [loading, setLoading] = useState(false);
-  const[disable,setDisable]=useState(false)
+  const [disable, setDisable] = useState(false)
 
   const exportToExcelFn = async () => {
-    
+
     setLoading(true)
     setDisable(true)
-    await axiosInstanceJWT.get(`${API_URL?.Export_FOR_MERCHANT_LIST}${props.URL}`,{
-      responseType: 'arraybuffer'
-    }).then((res) => {
+    await axiosInstanceJWT.get(`${API_URL?.Export_FOR_MERCHANT_LIST}${props.URL}`).then((res) => {
       if (res.status === 200) {
         const data = res?.data;
-         setLoading(false)
-         setDisable(false)
-        const blob = new Blob([data], {
-          type:
-            "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
-        });
-        const url = window.URL.createObjectURL(blob);
-        const a = document.createElement("a");
-        a.href = url;
-        a.download = `${ props?.filename}.xlsx`;
-        a.click();
-        window.URL.revokeObjectURL(url);
-       
-       
+
+        if (data.status) {
+          window.open(
+            data?.download_url,
+            '_top'
+            // '_blank' // <- This is what makes it open in a new window.
+          );
+        } else {
+          toastConfig.warningToast("Server Error, File not found")
+        }
+        setLoading(false)
+        setDisable(false)
+
+        // window.location.href = ""
+
+        // const a = document.createElement("a");
+        // a.href = data?.download_url;
+        // a._target = "_blank"
+        // // a.download = `${props?.filename}.xlsx`;
+        // a.click();
+        // window.URL.revokeObjectURL(data?.download_url);
+
+
 
       }
-    }).catch((err)=>{
-      if(err.response?.status===500){
-        toastConfig.errorToast("Something went wrong.")
-      }
+    }).catch((err) => {
+      toastConfig.errorToast("Something went wrong.")
       setLoading(false)
       setDisable(false)
 
@@ -46,19 +51,19 @@ const MerchnatListExportToxl = (props) => {
 
   return (
     <div className='form-group col-lg-3 col-md-12'>
-      
-        <p>&nbsp;</p>
-        <button
-          className="btn btn-sm text-white  cob-btn-primary"
-          disabled={disable}
-          type="button"
-          onClick={() => exportToExcelFn()}
-          style={{ backgroundColor: "rgb(1, 86, 179)" }}
-        >
-         <i className="fa fa-download"/> {loading ? "Downloading..." : "Export"}
-         
-        </button>
-     
+
+      <p>&nbsp;</p>
+      <button
+        className="btn btn-sm text-white  cob-btn-primary"
+        disabled={disable}
+        type="button"
+        onClick={() => exportToExcelFn()}
+        style={{ backgroundColor: "rgb(1, 86, 179)" }}
+      >
+        <i className="fa fa-download" /> {loading ? "Downloading..." : "Export"}
+
+      </button>
+
 
     </div>
   )
