89fb40907b43c9718c452b18716aaa661cbcd3dc
moneyverma <46442522+moneyverma@users.noreply.github.com>
2024-07-09 14:34:55 +0530

    feat: payment link UI fixed

--- src/components/dashboard/AllPages/createpaymentlink/Paylink.js
+++ src/components/dashboard/AllPages/createpaymentlink/Paylink.js
@@ -1,28 +1,13 @@
 import React, { useState } from 'react';
-import PayerDetails from './createpaymentlink/PayerDetails';
-import PaymentLinkDetail from './createpaymentlink/PaymentLinkDetail';
-import BulkPayer from './createpaymentlink/BulkPayer';
-import Reports from './createpaymentlink/Reports';
-import { useSelector } from 'react-redux';
-import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
+import PayerDetails from './PayerDetails';
+import PaymentLinkDetail from './PaymentLinkDetail';
+import BulkPayer from './BulkPayer';
+import Reports from './Reports';
+import classes from './paylink.module.css'
 
 
 function Paylink() {
   const [tab, SetTab] = useState(1);
-
-  const { auth } = useSelector((state) => state);
-  const { user } = auth;
-  let history = useHistory();
-
-
-  if (user.roleId !== 3 && user.roleId !== 13) {
-    if (user.clientMerchantDetailsList === null) {
-      history.push('/dashboard/profile');
-
-    }
-  }
-
-
   return (
     <section className="">
       <main className="">
@@ -34,15 +19,15 @@ function Paylink() {
             <div className="container-fluid">
               <div className="row">
                 <div className="col-lg-12 my-4 pl-3">
-                  <ul className="nav nav-tabs flex-nowrap">
+                  <ul className={`${classes.paylink_tabs} nav nav-tabs flex-nowrap`}>
                     <li className="nav-item" onClick={() => SetTab(1)}>
-                      <a href={() => false} id="navpad" className={"nav-link btn rounded-0 border border-secondary m-1 " + (tab === 1 ? "active" : "inactive")}  >Payment Detail</a>
+                      <a href={() => false} id="navpad" className={"nav-link btn rounded-1 border border-secondary m-1 " + (tab === 1 ? "active" : "inactive")}  >Payment Detail</a>
                     </li>
                     <li className="nav-item" onClick={() => SetTab(2)} >
-                      <a href={() => false} id="navpad" className={"nav-link btn rounded-0 border border-secondary m-1 " + (tab === 2 ? "active" : "inactive")} >Payment Link Detail</a>
+                      <a href={() => false} id="navpad" className={"nav-link btn rounded-1 border border-secondary m-1 " + (tab === 2 ? "active" : "inactive")} >Payment Link Detail</a>
                     </li>
                     <li className="nav-item" onClick={() => SetTab(4)}>
-                      <a href={() => false} id="navpad" className={"nav-link btn rounded-0 border border-secondary m-1 " + (tab === 4 ? "active" : "inactive")} >Reports</a>
+                      <a href={() => false} id="navpad" className={"nav-link btn rounded-1 border border-secondary m-1 " + (tab === 4 ? "active" : "inactive")} >Reports</a>
                     </li>
                   </ul>
                 </div>
