537d2bd3aef3f1e9042fe0d35f749a9de73409f0
bhuvan joshi <bhuvanjoshi923@gmail.com>
2024-07-18 10:54:05 +0530

    made service file for merchant-kyc

--- src/services/kyc/merchant-kyc.js
+++ src/services/kyc/merchant-kyc.js
@@ -12,8 +12,88 @@ const updateContactInfo = (requestParam) => {
     return axiosInstanceJWT.put(`${API_URL.Save_General_Info}`, requestParam)
 }
 
+const otpForContactInfo = (requestParam) => {
+    return axiosInstanceJWT.post(`${API_URL.Send_OTP}`, requestParam)
+
+}
+const businessType = (requestParam) => {
+    return axiosInstanceJWT.get(`${API_URL.Business_type}`, requestParam)
+
+}
+const busiCategory = (requestParam) => {
+    return axiosInstanceJWT.get(`${API_URL.Business_Category_CODE}`, requestParam)
+
+}
+const platformType = (requestParam) => {
+    return axiosInstanceJWT.get(`${API_URL.Platform_type}`, requestParam)
+}
+const saveBusinessInfo = (requestParam) => {
+    return axiosInstanceJWT.put(`${API_URL.save_Business_Info}`, requestParam)
+}
+
+const businessOverviewState = (requestParam) => {
+    return axiosInstanceJWT.get(`${API_URL.Business_overview_state_}`)
+
+}
+const businessOverviewStatePostApi = () => {
+    return axiosInstanceJWT.post(`${API_URL.Business_overview_state_}`)
+
+}
+const saveMerchantInfo = (requestParam) => {
+    return axiosInstanceJWT.post(`${API_URL.SAVE_MERCHANT_INFO}`, requestParam)
+}
+const saveMerchantBankDetais = (requestParam) => {
+    return axiosInstanceJWT.put(
+        `${API_URL.Save_Settlement_Info}`,
+        requestParam)
+}
+const kycBankNames=()=>{
+    return axiosInstanceJWT.get(`${API_URL.GET_ALL_BANK_NAMES}`)
+
+}
+const documentsUpload=(data)=>{
+    const businessType = data?.businessType;
+        const is_udyam = data?.is_udyam;
+    return axiosInstanceJWT.get(`${API_URL?.DocumentsUpload}/?business_type_id=${businessType}&is_udyam=${is_udyam}`)
+
+
+}
+const GetKycTabsStatus=(requestParam)=>{
+    return axiosInstanceJWT.get(`${API_URL.KYC_TAB_STATUS_URL}/${requestParam?.login_id}`)
+}
+const kycDocumentUploadList=(requestParam)=>{
+    return axiosInstanceJWT.post(`${API_URL?.DOCUMENT_BY_LOGINID}`, requestParam)
+}
+
+const merchantInfo = (requestParam) => {
+    console.log("requestParam",requestParam)
+    const url = requestParam.docType === "1"
+        ? API_URL.UPLOAD_MERCHANT_AADHAAR
+        : API_URL.upload_Single_Doc;
+
+    return axiosInstanceJWT.post(url, requestParam.bodyFormData, {
+        headers: { "Content-Type": "multipart/form-data" }
+    });
+};
+
+
+
 export const merchantKycService = {
-    updateContactInfo
+    updateContactInfo,
+    otpForContactInfo,
+    businessType,
+    busiCategory,
+    platformType,
+    saveBusinessInfo,
+    businessOverviewState,
+    businessOverviewStatePostApi,
+    saveMerchantInfo,
+    saveMerchantBankDetais,
+    kycBankNames,
+    documentsUpload,
+    GetKycTabsStatus,
+    kycDocumentUploadList,
+    merchantInfo
 }
 
 
