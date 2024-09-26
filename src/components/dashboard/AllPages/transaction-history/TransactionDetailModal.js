c6cbfbc044f6425da1f63a5d27a424a290beb8be
moneyverma <46442522+moneyverma@users.noreply.github.com>
2024-09-26 12:53:38 +0530

    Feat: Add Transaction Detail Modal

--- src/components/dashboard/AllPages/transaction-history/TransactionDetailModal.js
+++ src/components/dashboard/AllPages/transaction-history/TransactionDetailModal.js
@@ -1,77 +1,28 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import classes from "./transaction.module.css"
 
-function TransactionDetailModal() {
+function TransactionDetailModal({ fnSetModalToggle, transactionData }) {
 
     const [udfToggle, setUdfToggle] = useState(false)
+    useEffect(() => {
 
-    const transactionData = {
-        "srNo": 23,
-        "txn_id": "127662305240883560",
-        "client_txn_id": "TESTING230524084736727",
-        "challan_no": null,
-        "payee_amount": "21.00",
-        "trans_date": "2024-05-23 20:47:50",
-        "trans_complete_date": "2024-05-23 20:47:57",
-        "status": "REFUND_INITIATED",
-        "pag_response_code": "NA",
-        "resp_msg": "Your Transaction is Complete",
-        "payee_first_name": "laxmikantyadav",
-        "payee_lst_name": "NA",
-        "payee_mob": "NA",
-        "payee_email": "techsupport@srslive.in",
-        "client_code": "LPSD1",
-        "payment_mode": "Credit Card",
-        "payee_address": "NA",
-        "udf1": "1",
-        "udf2": "2",
-        "udf3": "3",
-        "udf4": "4",
-        "udf5": "5",
-        "udf6": "6",
-        "udf7": "7",
-        "udf8": "8",
-        "udf9": 9,
-        "udf10": 10,
-        "udf11": 11,
-        "udf12": 12,
-        "udf13": 13,
-        "udf14": 14,
-        "udf15": 15,
-        "udf16": 16,
-        "udf17": 17,
-        "udf18": 18,
-        "udf19": 19,
-        "udf20": 20,
-        "client_name": "Canara Bank Demo",
-        "gr_number": null,
-        "paid_amount": "21.00",
-        "pg_pay_mode": "SabpaisaBank",
-        "act_amount": "21.00",
-        "bank_message": "Transaction is successfully done !",
-        "fee_forward": "no",
-        "encrypted_pan": "NA",
-        "ifsc_code": null,
-        "payer_acount_number": null,
-        "bank_txn_id": "sp5443325490463",
-        "pg_return_amount": "21.0",
-        "p_convcharges": "4.0",
-        "p_ep_charges": "0.23",
-        "p_gst": "0.0"
-    }
+        return () => {
+            setUdfToggle(false)
+        }
+    }, [])
 
     return (
         <div className="modal fade mymodals show d-flex justify-content-end">
-            <div className={`modal-dialog modal-xl m-0 ${classes.z_index_99999}`} >
+            <div className={`modal-dialog modal-xl m-0 ${classes.z_index_99999} `} >
                 <div className="modal-content rounded-0">
                     <div className="modal-header">
-                        <h1 className="modal-title fs-5" id="exampleModalLabel">Sabpaisa Transaction ID : 123412341234</h1>
-                        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" />
+                        <h1 className="modal-title fs-5 border-start border-5 ps-2">Sabpaisa Transaction ID : <span className='text-primary'>123412341234</span></h1>
+                        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" onClick={() => fnSetModalToggle(false)} />
                     </div>
                     <div className="modal-body">
                         <div className="mb-5">
                             <h6>Transaction Details</h6>
-                            <table className="table table-striped-columns table-bordered">
+                            <table className={`table table-striped-columns table-bordered ${classes.table_layout_fixed}`}>
                                 <tbody>
                                     <tr>
                                         <th>Client Transaction ID</th>
@@ -104,7 +55,7 @@ function TransactionDetailModal() {
 
                         <div className="mb-5">
                             <h6>Payer Details</h6>
-                            <table className="table table-striped-columns table-bordered">
+                            <table className={`table table-striped-columns table-bordered ${classes.table_layout_fixed}`}>
                                 <tbody>
                                     <tr>
                                         <th>First Name</th>
@@ -132,7 +83,7 @@ function TransactionDetailModal() {
 
                         <div className="mb-5">
                             <h6>Merchant Specific Parameters</h6>
-                            <table className="table table-striped-columns table-bordered">
+                            <table className={`table table-striped-columns table-bordered ${classes.table_layout_fixed}`}>
                                 <tbody>
                                     <tr>
                                         <th>UDF1</th>
@@ -220,12 +171,11 @@ function TransactionDetailModal() {
                                         </React.Fragment>
 
                                     }
-
-
                                     <tr className="cursor_pointer" onClick={() => setUdfToggle(prev => !prev)}>
                                         <td colSpan={2}>
-                                            <p className="m-0 text-center font-bold">{udfToggle ? 'Hide UDF Values' : 'Show UDF Values'}   </p></td></tr>
-
+                                            <p className="m-0 text-center font-bold">{udfToggle ? 'Hide UDF Values' : 'Show UDF Values ...'}</p>
+                                        </td>
+                                    </tr>
                                 </tbody>
                             </table>
                         </div>
