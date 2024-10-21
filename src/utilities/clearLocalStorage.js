465240b134ada3b55350cb95e7116cc4b4b59c5e
Neha <neha.sinha@sabpaisa.in>
2024-10-21 16:59:28 +0530

    clear local storage for referrer onboarding on logout

--- src/utilities/clearLocalStorage.js
+++ src/utilities/clearLocalStorage.js
@@ -1,13 +1,14 @@
 export const clearLocalStore = () => {
-    localStorage.removeItem("subscriptionplan")
-    localStorage.removeItem("subscriptionchargesdetail")
-    localStorage.removeItem("at")
-    localStorage.removeItem("rt")
-    localStorage.removeItem("categoryId")
-    localStorage.removeItem("user")
-    localStorage.removeItem("authToken")
-    localStorage.removeItem("ap")
-    localStorage.removeItem("mid")
-    localStorage.removeItem("onboardingStatusByAdmin")
-    localStorage.removeItem("expiredTime")
-}
+  localStorage.removeItem("subscriptionplan");
+  localStorage.removeItem("subscriptionchargesdetail");
+  localStorage.removeItem("at");
+  localStorage.removeItem("rt");
+  localStorage.removeItem("categoryId");
+  localStorage.removeItem("user");
+  localStorage.removeItem("authToken");
+  localStorage.removeItem("ap");
+  localStorage.removeItem("mid");
+  localStorage.removeItem("onboardingStatusByAdmin");
+  localStorage.removeItem("expiredTime");
+  localStorage.removeItem("onboardingStatusByReferrer");
+};
