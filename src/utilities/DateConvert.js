ba7df820396545551c648377f48628013b408948
Neha <neha.sinha@sabpaisa.in>
2024-10-22 17:41:06 +0530

    update settlement report table

--- src/utilities/DateConvert.js
+++ src/utilities/DateConvert.js
@@ -14,32 +14,29 @@ const DateFormatter = (date, time = true) => {
   return <span>{formattedDate}</span>;
 };
 
-
 export const dateFormatBasic = (dateVal) => {
-  //convert only this format 2024-10-10T12:36:30Z 
-
+  //convert only this format 2024-10-10T12:36:30Z
+  if (!dateVal.includes("T")) return dateVal;
   let date;
-  console.log("dateVal", dateVal)
-  if ((dateVal === null && isNaN(date)) || dateVal === 'NA') {
-    date = "N/A"
+  // console.log("dateVal", dateVal)
+  if ((dateVal === null && isNaN(date)) || dateVal === "NA") {
+    date = "N/A";
   } else {
     // Extract date components
     const sdate = new Date(dateVal);
-    const day = String(sdate?.getUTCDate()).padStart(2, '0');
-    const month = String(sdate?.getUTCMonth() + 1).padStart(2, '0'); // Months are zero-based
+    const day = String(sdate?.getUTCDate()).padStart(2, "0");
+    const month = String(sdate?.getUTCMonth() + 1).padStart(2, "0"); // Months are zero-based
     const year = sdate?.getUTCFullYear();
 
     // Extract time components
-    const hours = String(sdate?.getUTCHours()).padStart(2, '0');
-    const minutes = String(sdate?.getUTCMinutes()).padStart(2, '0');
-    const seconds = String(sdate?.getUTCSeconds()).padStart(2, '0');
-
+    const hours = String(sdate?.getUTCHours()).padStart(2, "0");
+    const minutes = String(sdate?.getUTCMinutes()).padStart(2, "0");
+    const seconds = String(sdate?.getUTCSeconds()).padStart(2, "0");
     // Format the date and time
     date = `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
   }
 
   return date;
 };
-
 
 export default DateFormatter;
