ff51960f84fec0eeece5ca9eba1b260a5785723e
moneyverma <46442522+moneyverma@users.noreply.github.com>
2024-12-16 12:29:21 +0530

    feat: display input field error message

--- src/components/KYC/ContactInfoKyc.js
+++ src/components/KYC/ContactInfoKyc.js
@@ -522,12 +522,13 @@ function ContactInfoKyc(props) {
         {({ values, errors, isValid, setFieldError, setFieldValue }) => (
           <Form>
             <div className="row">
+              {console.log("errors", errors)}
               <div className="col-lg-6 col-sm-12 col-md-12">
                 <label
                   className="col-form-label mt-0 p-2"
                   data-tip={tooltipData.contact_person_name}
                 >
-                  Contact Person Name<span className="text-danger"> *</span>
+                  Contact Person Name<span className="text-danger">-- *</span>
                 </label>
                 <Field
                   type="text"
@@ -535,6 +536,10 @@ function ContactInfoKyc(props) {
                   className="form-control"
                   disabled={VerifyKycStatus === "Verified" ? true : false}
                 />
+                <ErrorMessage name="name">
+                  {(msg) => <p className="text-danger m-0">{msg}</p>}
+                </ErrorMessage>
+
               </div>
 
               <div className="col-lg-6 col-sm-12 col-md-12">
