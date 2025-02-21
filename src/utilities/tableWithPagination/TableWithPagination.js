c38d5d6dcd0472e298216a88fe2bc3a35fa12c9e
Abhishek Verma <abh69189@gmail.com>
2025-02-21 11:44:13 +0530

    refactor: file

--- src/utilities/tableWithPagination/TableWithPagination.js
+++ src/utilities/tableWithPagination/TableWithPagination.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import ReactPaginate from 'react-paginate';
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 // import { faSearch, faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons';
