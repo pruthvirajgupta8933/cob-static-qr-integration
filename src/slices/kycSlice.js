51bae3c4e87fef7c0bb13673f0c92ebd123cfd0e
Abhishek Verma <abh69189@gmail.com>
2025-03-24 16:46:24 +0530

    updt: pass masking value / swap the paylink graph filter

--- src/slices/kycSlice.js
+++ src/slices/kycSlice.js
@@ -396,8 +396,9 @@ export const merchantInfo = createAsyncThunk(
 export const kycUserList = createAsyncThunk(
   "kyc/kycUserList",
   async (requestParam) => {
+    const operation = requestParam?.masking === 1 ? "r" : "k"
     const response = await axiosInstanceJWT
-      .post(`${API_URL.Kyc_User_List}`, requestParam)
+      .post(`${API_URL.Kyc_User_List}`, { ...requestParam, operation: operation })
       .catch((error) => {
         return error.response;
       });
