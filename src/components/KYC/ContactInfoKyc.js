4a984055ef35b5936811eb30e1fd1189000b8b08
Bhuvan Chandra Joshi <bhuvanjoshi923@gmail.com>
2025-03-25 15:47:40 +0530

    contactinfo kyc remove anchor tag

--- src/components/KYC/ContactInfoKyc.js
+++ src/components/KYC/ContactInfoKyc.js
@@ -467,8 +467,9 @@ function ContactInfoKyc(props) {
         ) : (
           <div className="input-group-append">
             {idType === "1" && (
-              <a
-                href={() => false}
+              <button
+                // href={() => false}
+                type='button'
                 className={`btn cob-btn-primary btn-sm ${values.id_number?.length < 12 ||
                   aadhaarVerificationLoader ||
                   errors?.id_number
@@ -487,11 +488,12 @@ function ContactInfoKyc(props) {
                 ) : (
                   "Verify"
                 )}
-              </a>
+              </button>
             )}
             {idType === "3" && (
-              <a
-                href={() => false}
+              <button
+                // href={() => false}
+                type='button'
                 className={`btn cob-btn-primary btn-sm ${values.id_number?.length < 10 ? "disabled" : ""
                   }`}
                 onClick={() => {
@@ -506,11 +508,12 @@ function ContactInfoKyc(props) {
                 ) : (
                   "Verify"
                 )}
-              </a>
+              </button>
             )}
             {idType === "4" && (
-              <a
-                href={() => false}
+              <button
+                // href={() => false}
+                type='button'
                 className={`btn cob-btn-primary btn-sm ${values.id_number?.length < 14 || errors?.id_number
                   ? "disabled"
                   : ""
@@ -520,7 +523,7 @@ function ContactInfoKyc(props) {
                 }}
               >
                 Verify
-              </a>
+              </button>
             )}
           </div>
         )}
@@ -770,8 +773,9 @@ function ContactInfoKyc(props) {
                     </span>
                   ) : (
                     <div className="input-group-append">
-                      <a
-                        href={() => false}
+                      <button
+                        // href={() => false}
+                        type='button'
                         className={`btn cob-btn-primary btn-sm text-white ${isLoading || KycList?.isEmailVerified !== 1 ? "disabled" : ""
                           }`}
                         onClick={() => {
@@ -794,7 +798,7 @@ function ContactInfoKyc(props) {
                         ) : (
                           "Send OTP"
                         )}
-                      </a>
+                      </button>
                     </div>
                   )}
                 </div>
