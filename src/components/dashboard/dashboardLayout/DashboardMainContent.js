73ea3e3a41924839ddb0ed8449a72b015c22fd37
Bhuvan Chandra Joshi <bhuvanjoshi923@gmail.com>
2025-04-02 14:25:50 +0530

    new component disbursment added

--- src/components/dashboard/dashboardLayout/DashboardMainContent.js
+++ src/components/dashboard/dashboardLayout/DashboardMainContent.js
@@ -123,6 +123,7 @@ import { assignmentTypeApi, setAssignmentType } from "../../../slices/assign-acc
 import Mfa from "../../ApproverNVerifier/Mfa/Mfa";
 import AssigneBusinessDevelopment from "../../ApproverNVerifier/AssignBusinessDevelopment/AssignBusinessDevelopment";
 import UpdateRollingReserve from "../../ApproverNVerifier/UpdateRollingReserve/UpdateRollingReserve";
+import Disbursment from "../../ApproverNVerifier/Disbursment/Disbursment";
 
 function DashboardMainContent() {
   let history = useHistory();
@@ -1067,6 +1068,17 @@ function DashboardMainContent() {
                 Component={UpdateRollingReserve}
                 roleList={{ approver: true, verifier: true }}
               />
+
+
+              <AuthorizedRoute
+                exact
+                path={`${path}/disbursment`}
+                Component={Disbursment}
+                roleList={{ approver: true }}
+              >
+
+              </AuthorizedRoute>
+
 
               <Route path={`${path}/*`} component={UrlNotFound}>
                 <UrlNotFound />
