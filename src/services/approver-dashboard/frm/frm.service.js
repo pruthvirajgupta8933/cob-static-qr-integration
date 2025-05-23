f509f522af108db1feb3bb535b4a5b26ab3c1136
Bhuvan Chandra Joshi <bhuvanjoshi923@gmail.com>
2025-05-23 15:51:13 +0530

    factumdata api integration

--- src/services/approver-dashboard/frm/frm.service.js
+++ src/services/approver-dashboard/frm/frm.service.js
@@ -1,13 +1,18 @@
 import API_URL from "../../../config"
 import { axiosInstanceJWT } from "../../../utilities/axiosInstance"
 
-export const frmPushMerchantData=(obj)=>{
-    return axiosInstanceJWT.post(API_URL. PUSH_MERCHANT_DATA,obj)
+export const frmPushMerchantData = (obj) => {
+    return axiosInstanceJWT.post(API_URL.PUSH_MERCHANT_DATA, obj)
 }
 
-export const frmMerchantList=(obj)=>{
-    const pageSize=obj.page_size
-    const page=obj.page
+export const frmSingleScreeningApi = (obj) => {
+    return axiosInstanceJWT.post(API_URL.FRM_SINGLE_SCREENING, obj)
+
+}
+
+export const frmMerchantList = (obj) => {
+    const pageSize = obj.page_size
+    const page = obj.page
     const url = `${API_URL.MERCHANT_FRM_LIST}?page_size=${pageSize}&page=${page}`
     return axiosInstanceJWT.post(url)
 }
