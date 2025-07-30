2ca7c73c163ac898db5d9e3ab706faf889b2a8e3
Abhishek Verma <abh69189@gmail.com>
2025-07-30 13:20:16 +0530

    remove console

--- src/components/mainComponent/login/Login.js
+++ src/components/mainComponent/login/Login.js
@@ -105,7 +105,7 @@ const Login = () => {
         keyConfig.LOGIN_AUTH_IV
       ),
     };
-    console.log(encQuery)
+
 
     dispatch(login(encQuery)).then((res) => {
       if (res?.payload?.user?.status && res?.payload?.user?.is_mfa_enabled) {
