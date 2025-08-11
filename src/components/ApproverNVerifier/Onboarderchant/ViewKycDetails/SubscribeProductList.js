0b0fc0b29f8a0ea3ce40185c739627b998296c50
Abhishek Verma <abh69189@gmail.com>
2025-08-11 17:39:28 +0530

    update: display all subscription plans and handle null values

--- src/components/ApproverNVerifier/Onboarderchant/ViewKycDetails/SubscribeProductList.js
+++ src/components/ApproverNVerifier/Onboarderchant/ViewKycDetails/SubscribeProductList.js
@@ -1,16 +1,12 @@
-import React from 'react'
-
+// import React from 'react'
 function SubscribeProductList({ SubscribedPlanData }) {
 
-
-
-
-    const filterSubscibedData = SubscribedPlanData
-        ?.filter(
-            (item) =>
-                item?.status?.toLowerCase() === 'verified' &&
-                item?.subscription_status?.toLowerCase() === 'subscribed'
-        )
+    // const filterSubscibedData = SubscribedPlanData
+    //     ?.filter(
+    //         (item) =>
+    //             item?.status?.toLowerCase() === 'verified' &&
+    //             item?.subscription_status?.toLowerCase() === 'subscribed'
+    //     )
 
 
 
@@ -21,14 +17,32 @@ function SubscribeProductList({ SubscribedPlanData }) {
                 <table class="table mt-3">
                     <thead>
                         <tr>
+                            <th>Sno.</th>
                             <th>Product</th>
                             <th>Plan</th>
+                            <th>Purchased Amount (INR)</th>
                             <th>Status</th>
+                            <th>Subscription Status</th>
+
                         </tr>
                     </thead>
                     <tbody>
 
-                        {filterSubscibedData
+                        {SubscribedPlanData?.length > 0 ? SubscribedPlanData
+                            .map((item, index) => {
+                                return (
+                                    <tr key={index}>
+                                        <td>{index + 1}</td>
+                                        <td>{item?.applicationName}</td>
+                                        <td>{item?.planName}</td>
+                                        <td>{item?.purchaseAmount}</td>
+                                        <td>{item?.status}</td>
+                                        <td>{item?.subscription_status}</td>
+
+                                    </tr>
+                                )
+                            }) : <tr><td colSpan="3">No Subscribed Plans Found</td></tr>}
+                        {/* {filterSubscibedData
                             .map((item, index) => {
                                 // console.log("item", item.mandateStatus);
                                 return (item?.mandateStatus?.toLowerCase() === 'success' && item?.plan_code == '005') || item?.plan_code != '005' ? (
@@ -38,7 +52,7 @@ function SubscribeProductList({ SubscribedPlanData }) {
                                         <td>{item?.subscription_status}</td>
                                     </tr>
                                 ) : <></>;
-                            })}
+                            })} */}
 
                     </tbody>
                 </table>
